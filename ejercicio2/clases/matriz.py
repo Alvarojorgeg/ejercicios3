28ec73b31fce109cf9517a85fadce73b874a6aa7
Álvaro <145665238+Alvarojorgeg@users.noreply.github.com>
2025-04-03 17:24:06 +0000

    ejercicio2 FUNCIONANDO :)

--- ejercicio2/clases/matriz.py
+++ ejercicio2/clases/matriz.py
@@ -1,3 +1,30 @@
 class Matriz:
     def __init__(self, numeros):
         self.numeros = numeros
+
+    def det_recursivo(self, m=None):
+        if m is None:
+            m = self.numeros
+
+        if len(m) == 1:
+            return m[0][0]
+
+        if len(m) == 2:
+            return m[0][0] * m[1][1] - m[0][1] * m[1][0]
+
+        det = 0
+        for c in range(len(m)):
+            det += ((-1) ** c) * m[0][c] * self.det_recursivo(
+                [fila[:c] + fila[c+1:] for fila in m[1:]])
+        return det
+
+    def det_iterativo(self):
+        m = self.numeros
+        return (
+            m[0][0] * m[1][1] * m[2][2] +
+            m[0][1] * m[1][2] * m[2][0] +
+            m[0][2] * m[1][0] * m[2][1] -
+            m[0][2] * m[1][1] * m[2][0] -
+            m[0][0] * m[1][2] * m[2][1] -
+            m[0][1] * m[1][0] * m[2][2]
+        )
--- ejercicio2/main.py
+++ ejercicio2/main.py
@@ -1,2 +1,15 @@
 from clases.matriz import Matriz
 
+from clases.matriz import Matriz
+
+if __name__ == "__main__":
+    numeros = [
+        [4, 3, 2],
+        [1, 5, 7],
+        [6, 8, 9]
+    ]
+
+    matriz = Matriz(numeros)
+
+    print("Determinante (recursivo):", matriz.det_recursivo())
+    print("Determinante (iterativo):", matriz.det_iterativo())
